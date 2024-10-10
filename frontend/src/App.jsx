7b8fa92127dcb93ef4582972aa3cc29a96e6e740
Rafiq Shaik <rafiqshaik097@gmail.com>
2024-10-10 16:25:26 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Spinner from "./components/Spinner";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  if (loading) return <Spinner />;
+
   return (
-    <>
-      <Routes>
-        <Route
-          path="/login"
-          element={<LoginPage />}
-        />
-      </Routes>
+    <di>
+      <div className="flex flex-col min-h-screen">
+        <Routes>
+          <Route path="/" element={}/>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
